944e1de024cc9428edfb857be2149a14712dfd7e
Adam Kiwior <kivi.83@wp.pl>
2020-12-30 22:00:24 +0100

    dodane geojsona i kodu odczytujaco-wpisujaca wartosc do slownika

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 import slownik_kody as sl
 slownik = sl.teryt_load('slownik.csv')
 sl.convert_to_json(slownik, 'new.json')
-slownik2 = sl.load_json_dict('new.json')
-print(slownik2)
-
+slownik2 = sl.load_json_dict('probny_lot.geojson')
+slownik3 = sl.load_json_dict('new.json')
+slownik_feat = slownik2['features']   
+# print(slownik_feat[0])
+kod = 1
+for rekord in slownik_feat:
+    rekord_num = rekord['properties']
+    # print(rekord_num)
+    # print(rekord['properties'])
+    rekord_num['nowsze'] = kod
+    imie = rekord_num['IMIE']
+    print(imie)
+    insert = slownik3[imie]
+    print(f" to jest watrość wstawiona do pliku json{insert}")
+    rekord_num['nowsze'] = insert 
+    kod = kod + 1
+    print(rekord_num)
+    # for atr in rekord_num:
+    #     print(atr)
+    #     atr['nowsze'] = kod
+    # kod = kod + 1        
+    # print(rekord['properties'])
