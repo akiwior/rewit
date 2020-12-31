4a41cea828f60d2372b6160be592a972d65fb7d2
Adam Kiwior <akiwior@um.slupsk.pl>
2020-12-31 15:20:24 +0100

    dodanie rekorud do teryt.csv oraz sekcji do importu kodow teryt

--- load_teryr.py
+++ load_teryr.py
@@ -1,19 +1,36 @@
-# import json
+import json
 import csv
-# filename = "E:\\rewit\\pkt_adresowe.json" 
-# slownik = {}
+from slownik_kody import *
+filename = "pkt_adresowe.geojson" 
+# slownik1 = {}
 # with open(filename, encoding = 'utf-8') as f:
-#     slownik = json.load(f)
-# print(slownik)
-# from slownik_kody import *
-# adresy = load_json_dict("E:\\rewit\\pkt_adresowe.geojson")
+#     slownik1 = json.load(f)
+# print(slownik1)
+
+adresy = load_json_dict("E:\\rewit\\pkt_adresowe.geojson")
+slownik_feat = adresy['features']
 # print(adresy)
 plik = r"ULIC_SIMC(0977278)_TERC(2263011)_31-12-2020.csv"
 f = open(plik , 'r', encoding="utf-8", newline='')
 reader = csv.reader(f, delimiter=';', quotechar='|')
 slownik = {}
 for row in reader:
-    print(row)
+    # print(row)
     slownik[row[7]] = row[5]
 # return slownik
-print(slownik)
+# print(slownik)
+for rekord in slownik_feat:
+    rekord_num = rekord['properties']
+    ulica = (rekord_num['ulica'])
+    # print(f"{ulica} {slownik[ulica]}")
+    rekord_num['teryt'] = slownik[ulica]
+    # # print(rekord_num)
+    # # print(rekord['properties'])
+    # rekord_num['nowsze'] = kod
+    # imie = rekord_num['IMIE']
+    # # print(imie)
+    # insert = slownik3[imie]
+    # print(f" to jest watrość wstawiona do pliku json {insert}")
+    # rekord_num['nowsze'] = insert 
+    # kod = kod + 1
+    # print(rekord_num)
