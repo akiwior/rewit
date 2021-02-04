b20963e2779eb0b928095f1061b4abf27069b80f
Adam Kiwior <kivi.83@wp.pl>
2021-02-05 00:05:05 +0100

    test.py dziala z gdalem

---
+++ gdal.py
@@ -0,0 +1,4 @@
+import gdal
+gdal.RaterizeLayer()
+
+
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from funkcje_kody import *
+
 # importowanie slownika z pliku csv 1 kolumna ulica, kolumna 0 to kod teryt
 teryt = teryt_load('csv/ulice_kod_full.csv', 1, 0, ',')
 #importowanie z jsona punktow adresowych-geojson
@@ -10,5 +11,11 @@ dane = ulice_dane_load_csv("csv/ulica_numer_dana.csv",0, 1, 2, ',', teryt)
 # dodanie teryt do adresow
 adres_teryt = add_teryt_to_adresy_geojson(adresy, teryt)
 #dodanie danych do adresow
-adres_teryt_
+adres_teryt_data = add_data_to_ulice(adres_teryt, dane,0)
+#dodanie id siatki do kazdego punktu adresowego
+adres_teryt_data_grid = dod_do_adres_grid_id(adres_teryt_data, grid)
+#dodanie danych data do siatki
+grid_data = add_data_to_gird(grid, adres_teryt_data_grid)
+#zapisanie geojson nowy_slownik_tekstowy.geojson
+convert_to_json(grid_data, 'nowy_slownik_testowy.geojson')
 
